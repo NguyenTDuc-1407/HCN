e68f85c83023a6fa9ee80df0f2a43a6ef1a33248
Duc.NT <311020ntd@gmail>
2024-08-05 19:09:30 +0700

    area

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿float height;
+float width;
+Console.WriteLine("input height");
+height = float.Parse(Console.ReadLine());
+Console.WriteLine("input width");
+width = float.Parse(Console.ReadLine());
+float area = height*width;
+Console.WriteLine("area " + area);
